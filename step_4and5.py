56b938c300f0cce929a58e1f0a6e6225cf9745f6
ZmMurad <45798398+ZmMurad@users.noreply.github.com>
2023-02-11 01:29:45 +0500

    Update step_4and5.py
    Fix

--- step_4and5.py
+++ step_4and5.py
@@ -1,8 +1,11 @@
 import requests
+host_name = "https://wttr.in/"
+parametrs = {"n": "", "T": "", "q": "", "M": "", "lang": "ru"}
 
-geolocations=["https://wttr.in/svo?n&m&TMq&lang=ru","https://wttr.in/london?n&m&MTq&lang=ru","https://wttr.in/cherepovets?n&m&MTq&lang=ru"]
+geo_locations = ["svo",
+                "london", "cherepovets"]
 
-for url in geolocations:
-  response=requests.get(url)
-  response.raise_for_status()
-  print(response.text)
+for url in geo_locations:
+    response = requests.get(host_name+url, params=parametrs)
+    response.raise_for_status()
+    print(response.text)
